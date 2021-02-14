3d2a486a04dca9d927e5f4f252100b72bd21cddc
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-14 10:28:35 -0700

    Create action create

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -3,4 +3,21 @@ class ApplicantsController < ApplicationController
   def show
     @applicant = Application.find(params[:applicant_id])
   end
+
+  def new
+  end
+
+  def create
+    applicant = Application.new({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      application_status: params[:application_status],
+      })
+
+    applicant.save
+    redirect_to "/applicants/#{applicant.id}"
+  end
 end
