f869cec6ba052f7fb2c5a22e93ed2a7ce8a36602
Adam Bowers <adam.j.bowers@gmail.com>
2021-02-17 18:40:03 -0700

    Add test

--- spec/features/admins/index_spec.rb
+++ spec/features/admins/index_spec.rb
@@ -14,7 +14,10 @@ RSpec.describe 'Admins index page' do
     @dog_4 = create(:pet, name: "Zeeb")
     @dog_5 = create(:pet, name: "Zeel")
     @shelter_1 = create(:shelter)
-    @shelter_2 = create(:shelter, name: "" )
+    @shelter_2 = create(:shelter, name: "A Shelter")
+    @shelter_3 = create(:shelter, name: "Big Shelter")
+    @shelter_4 = create(:shelter, name: "City Shelter")
+    @shelter_5 = create(:shelter, name: "Dog Shelter")
     @app_1_dog_1 = PetApplication.create!(application: @app_1, pet: @dog_1)
     @app_1_dog_3 = PetApplication.create!(application: @app_1, pet: @dog_3)
     @app_2_dog_2 = PetApplication.create!(application: @app_2, pet: @dog_2)
@@ -28,21 +31,23 @@ RSpec.describe 'Admins index page' do
     @app_5_dog_4 = PetApplication.create!(application: @app_5, pet: @dog_4)
     @app_5_dog_5 = PetApplication.create!(application: @app_5, pet: @dog_5)
   end
-
-  it "displaces all shelters in the system in reverse alphabetical order" do
-    visit "/admin/applicants/#{@app_1.id}"
-
-    expect(page).to have_content("Pet Name: #{@dog_1.name}")
-    expect(page).to have_content("Pet Name: #{@dog_3.name}")
-
-    within("#pet-#{@dog_1.id}") do
-      expect(page).to have_button("Approve")
-    end
-
-    within("#pet-#{@dog_3.id}") do
-      expect(page).to have_button("Approve")
-    end
-
-    find(id: "Approve#{@dog_1.id}").click
-    find(id: "Approve#{@dog_3.id}").click
-  end
+end
+  # it "displaces all shelters in the system in reverse alphabetical order" do
+  #   visit "/admin/shelters"
+  #
+  #   within() do
+  #
+  #
+  #   end
+  #
+  #   within("#pet-#{@dog_1.id}") do
+  #     expect(page).to have_button("Approve")
+  #   end
+  #
+  #   within("#pet-#{@dog_3.id}") do
+  #     expect(page).to have_button("Approve")
+  #   end
+  #
+  #   find(id: "Approve#{@dog_1.id}").click
+  #   find(id: "Approve#{@dog_3.id}").click
+  # end
